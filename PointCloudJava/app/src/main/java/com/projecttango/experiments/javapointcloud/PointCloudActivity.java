1a9037083307aad4397c91a0f87243d8eeabf10b
Pablo Rodriguez Manzi <pablorm@creativa77.com.ar>
2015-06-19 17:00:11 -0300

    Added high accuracy adf trying to reproduce tango maps creator

--- PointCloudJava/app/src/main/java/com/projecttango/experiments/javapointcloud/PointCloudActivity.java
+++ PointCloudJava/app/src/main/java/com/projecttango/experiments/javapointcloud/PointCloudActivity.java
@@ -33,6 +33,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -45,6 +46,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * Simple Activity that exercises the Tango code and nothing else
@@ -62,6 +64,24 @@ public class PointCloudActivity extends Activity {
     public static Object poseLock = new Object();
     public static Object depthLock = new Object();
 
+    private Handler mHandler = new Handler();
+    private static final int POSE_POLLING_PERIOD_MS = 33;
+    private boolean mIsLocalized =false;
+    private double mLastLocalizedTimestamp = 0;
+
+    private static final TangoCoordinateFramePair ADF_DEVICE_FRAME_PAIR =
+            new TangoCoordinateFramePair(
+                    TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                    TangoPoseData.COORDINATE_FRAME_DEVICE);
+    private static final TangoCoordinateFramePair START_DEVICE_FRAME_PAIR =
+            new TangoCoordinateFramePair(
+                    TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                    TangoPoseData.COORDINATE_FRAME_DEVICE);
+    private static final TangoCoordinateFramePair ADF_START_FRAME_PAIR =
+            new TangoCoordinateFramePair(
+                    TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                    TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,7 +95,11 @@ public class PointCloudActivity extends Activity {
         mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
         mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_AUTORECOVERY, true);
         mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_LEARNINGMODE, true);
+        mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_EXPERIMENTAL_HIGH_ACCURACY_SMALL_SCALE_ADF,
+                true);
+        mConfig.putString(TangoConfig.KEY_STRING_AREADESCRIPTION, UUID.randomUUID().toString());
         mIsTangoServiceConnected = false;
+
     }
 
     @Override
@@ -116,6 +140,7 @@ public class PointCloudActivity extends Activity {
             }
             try {
                 setTangoListeners();
+                start();
             } catch (TangoErrorException e) {
                 Toast.makeText(this, R.string.TangoError, Toast.LENGTH_SHORT).show();
             } catch (SecurityException e) {
@@ -138,6 +163,48 @@ public class PointCloudActivity extends Activity {
         }
     }
 
+    private void start() {
+        mHandler.postDelayed(mGetPose, POSE_POLLING_PERIOD_MS);
+    }
+
+    private Runnable mGetPose = new Runnable() {
+        @Override
+        public void run() {
+            if (mTango != null) {
+                try {
+                    TangoPoseData pose;
+                    if (mIsLocalized) {
+                        pose = mTango.getPoseAtTime(0, ADF_DEVICE_FRAME_PAIR);
+                    } else {
+                        pose = mTango.getPoseAtTime(0, START_DEVICE_FRAME_PAIR);
+                    }
+                    if (TangoPoseData.POSE_VALID == pose.statusCode) {
+                        float[] translation = pose.getTranslationAsFloats();
+                        Log.i(TAG, "Device:  x: " + translation[0] + " y: " + translation[1] + " z: " + translation[2]);
+                    }
+
+
+
+
+                    pose = mTango.getPoseAtTime(0, ADF_START_FRAME_PAIR);
+                    if (TangoPoseData.POSE_VALID == pose.statusCode
+                            && pose.timestamp > mLastLocalizedTimestamp) {
+                        if (mLastLocalizedTimestamp != 0) {
+                            mIsLocalized = true;
+                            float[] translation2 = pose.getTranslationAsFloats();
+
+                            Log.i(TAG, "Relcoalized: x: "+ translation2[0] + " y: "+translation2[1] + " z: "+translation2[2]);
+                        }
+                        mLastLocalizedTimestamp = pose.timestamp;
+                    }
+                } catch (TangoInvalidException e) {
+                    Log.e(TAG, "Error Getting Pose", e);
+                }
+            }
+            mHandler.postDelayed(this, POSE_POLLING_PERIOD_MS);
+        }
+    };
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -145,16 +212,36 @@ public class PointCloudActivity extends Activity {
 
     private void setTangoListeners() {
         // Configure the Tango coordinate frame pair
-        final ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
         framePairs.add(new TangoCoordinateFramePair(
                 TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
                 TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE));
         // Listen for new Tango data
         mTango.connectListener(framePairs, new OnTangoUpdateListener() {
 
             @Override
-            public void onPoseAvailable(final TangoPoseData pose) {
+            public void onPoseAvailable(final TangoPoseData tangoPoseData) {
+                if(tangoPoseData.statusCode == TangoPoseData.POSE_VALID){
+                    if (tangoPoseData.baseFrame == TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION &&
+                            tangoPoseData.targetFrame == TangoPoseData.COORDINATE_FRAME_DEVICE) {
 
+
+
+
+                    } else if (tangoPoseData.baseFrame == TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION &&
+                            tangoPoseData.targetFrame == TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE) {
+                        Log.d(TAG, "Relocalized");
+
+                    }
+                }
             }
 
             @Override
